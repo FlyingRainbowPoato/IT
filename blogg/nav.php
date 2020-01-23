a381661012bc1a8dfe9da3aab6e93e4848a8389b
FlyingRainbowPoato <60003362+FlyingRainbowPoato@users.noreply.github.com>
2020-01-23 09:55:40 +0100

    Add files via upload

--- blogg/nav.php
+++ blogg/nav.php
@@ -2,10 +2,12 @@
   <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="new.php">New Post</a></li>
-    <li><a href="sign_up.php">Sign up</a></li>
+    <li class="signUpButton"><a href="sign_up.php">Sign up</a></li>
+    <li>
+      <?php 
+      if (!($_SERVER['REQUEST_URI'] == "/blogg/sign_up.php")) {
+        require_once "sign_in.php"; }
+      ?>
+    </li>
   </ul>
-  <?php 
-    if (!($_SERVER['REQUEST_URI'] == "/blogg/sign_up.php")) {
-      require_once "sign_in.php"; }
-  ?>
 </nav>
